84a1b1edd8dcdd51a533e50610c6e603facb08b5
zzde <zhangxh1997@gmail.com>
2018-06-29 16:46:55 +0800

    add par

--- cms/models.py
+++ cms/models.py
@@ -360,3 +360,35 @@ class ApplyUserTypes(models.Model):
 
     apply = models.ForeignKey(ApplyUser, on_delete=models.CASCADE)
     apply_type = models.ForeignKey(EventTypeDetail, on_delete=models.SET(-1))
+
+
+class UserParagraph(models.Model):
+    class Meta:
+        verbose_name = "用户段位认证"
+        verbose_name_plural = "用户段位认证"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="用户")
+    rz_date = models.DateTimeField(auto_now_add=True, verbose_name="认证时间")
+    paragraph = models.CharField(max_length=100, verbose_name="段位")
+
+
+class RzgParagraph(models.Model):
+    class Meta:
+        verbose_name = "认证官"
+        verbose_name_plural = "认证官"
+
+    country = models.CharField(max_length=50, verbose_name="地区")
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    sex = models.CharField(max_length=50, verbose_name="性别")
+    phone = models.BigIntegerField(verbose_name="电话")
+    wechat = models.CharField(max_length=100, verbose_name='微信')
+
+
+class JlParagraph(models.Model):
+    class Meta:
+        verbose_name = "教练证书"
+        verbose_name_plural = "教练证书"
+
+    country = models.CharField(max_length=50, verbose_name="地区")
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    paragraph = models.CharField(max_length=100, verbose_name="级别")
